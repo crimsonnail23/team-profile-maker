99cb11b87e757ed0bfda77a5f7eca18b5ee1c304
night <yseddiq@gmail.com>
2022-09-12 19:10:08 -0400

    created two arrays of questions, but only one gets activated

--- index.js
+++ index.js
@@ -2,16 +2,77 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 //creats an array of questions that will be asked.
-const questions = [
+const managerQuestionsArray = [
     {
         type: 'input',
-        name: 'employee name',
-        message: 'what is the name of the employee?'
+        name: 'name',
+        message: 'what is the name of the team manager?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'what is their ID'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'what is their email address'
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'what is their office number'
+    },
+    {
+        type: 'list',
+        name: 'addMore',
+        message: 'are you done building your team, or do you want to add more?',
+        choices: ['done', 'add engineer', 'intern'],
+        
     }
 ];
 
+const engineerQuestionsArray=[
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: 'what is the name of the engineer'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'what is their ID'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'what is their email'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'what is their username',
+    },
+    {
+        type: 'list',
+        name: 'addMore',
+        message: 'are you done building your team, or do you want to add more?',
+        choices: ['done', 'add engineer', 'intern']
+    }
+
+];
+
+function engineerQuestions(){
+    inquirer.prompt(engineerQuestionsArray)
+}
+
+//code that will start the prompts when called.
 function start(){
-    inquirer.prompt(questions)
+    inquirer.prompt(managerQuestionsArray)
+        .then((answers)=>{
+            console.log(answers)
+        });
 };
 
+//starts the prompts.
 start();
