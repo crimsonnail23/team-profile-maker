618f7fa335fab58ec7ea067b4a22c1e8074a4c5d
night <yseddiq@gmail.com>
2022-09-13 15:05:06 -0400

    fixed an issue where officeNumber was missing from the consturctor parenthesees

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,12 @@
 const Manager = require('../lib/Manager.js'); 
 
 test('checks to see if Manager inherited properties from Employee', ()=>{
-    const manager = new Manager('Joel',2,'joel@email.com');
+    const manager = new Manager('Joel',2,'joel@email.com', 821);
     
     expect(manager.name).toBe('Joel');
     expect(manager.id).toEqual(2);
     expect(manager.email).toBe('joel@email.com')
+    expect(manager.officeNumber).toEqual(821);
 });
 
 test('checks to see if getName method works',()=>{
@@ -26,7 +27,7 @@ test('checks to see if getEmail method works', ()=>{
     expect(manager.getEmail()).toBe('joel@email.com');
 })
 
-test('checks to see if officeNumber method works',()=>{
+test('checks to see if officeNumber method',()=>{
     const manager = new Manager('Joel',2,'joel@email.com');
 
     expect(manager.officeNumber()).toEqual(expect.any(Number));
