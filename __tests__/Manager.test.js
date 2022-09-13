feaabc018201c6055643ff036febd34fd34d59df
night <yseddiq@gmail.com>
2022-09-13 14:16:12 -0400

    added tests for Manager. only one that passed was the one that checked if Manager inherited properties from Employee class

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,27 @@
+const Manager = require('../lib/Employee.js'); 
+
+test('checks to see if Manager inherited properties from Employee', ()=>{
+    const manager = new Manager('Joel',2,'joel@email.com');
+    
+    expect(manager.name).toBe('Joel');
+    expect(manager.id).toEqual(2);
+    expect(manager.email).toBe('joel@email.com')
+});
+
+test('checks to see if getName method works',()=>{
+    const manager = new Manager('Joel',2,'joel@email.com');
+
+    expect(manager.getName).toBe('Joel');
+});
+
+test('checks to see if getId method works',()=>{
+    const manager = new Manager('Joel',2,'joel@email.com');
+    
+    expect(manager.getId).toEqual(2);
+});
+
+test('checks to see if getEmail method works', ()=>{
+    const manager = new Manager('Joel',2,'joel@email.com');
+
+    expect(manager.getEmail).toBe('joel@email.com');
+})
