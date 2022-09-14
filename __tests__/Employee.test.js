ea7ccc6e88dfa018818d5a0164526fa1207fa7bd
night <yseddiq@gmail.com>
2022-09-14 14:20:22 -0400

    refactored tests, they all pass. and started work on generating HTML

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,28 +6,8 @@ test('creates an employee class with some properties',()=>{
     expect(employee.name).toBe('Jennifer');
     expect(employee.id).toEqual(1);
     expect(employee.email).toBe('jenny@email.com');
-});
-
-test('checks to see if getName method works.',()=>{
-    const employee = new Employee('Jennifer', 1, 'jenny@email.com');
-    
     expect(employee.getName()).toBe('Jennifer');
-});
-
-test('checks to see if getId method works.',()=>{
-    const employee = new Employee('Jennifer', 1, 'jenny@email.com');
-
     expect(employee.getId()).toBe(1);
-});
-
-test('chceks to see if getEmail method works', ()=>{
-    const employee = new Employee('Jennifer', 1, 'jenny@email.com');
-
-    expect(employee.getEmail()).toBe('jenny@email.com')
-});
-
-test('checks to see if getRole method works.',()=>{
-    const employee = new Employee('Jennifer', 1, 'jenny@email.com');
-
+    expect(employee.getEmail()).toBe('jenny@email.com');
     expect(employee.getRole()).toBe('Employee');
-})
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,40 +1,17 @@
 const Manager = require('../lib/Manager.js'); 
 
-test('checks to see if Manager inherited properties from Employee and if officeNumber property was succesfully added', ()=>{
+test('checks to see if Manager inherited properties from Employee and if all methods work', ()=>{
     const manager = new Manager('Joel',2,'joel@email.com', 821);
     
     expect(manager.name).toBe('Joel');
     expect(manager.id).toEqual(2);
     expect(manager.email).toBe('joel@email.com')
     expect(manager.officeNumber).toEqual(821);
-});
-
-test('checks to see if getName method works',()=>{
-    const manager = new Manager('Joel',2,'joel@email.com');
-
     expect(manager.getName()).toBe('Joel');
-});
-
-test('checks to see if getId method works',()=>{
-    const manager = new Manager('Joel',2,'joel@email.com');
-    
     expect(manager.getId()).toEqual(2);
+    expect(manager.getEmail()).toBe('joel@email.com');
+    expect(manager.getOfficeNumber()).toEqual(821);
+    expect(manager.getRole()).toBe('Manager');
+    
 });
 
-test('checks to see if getEmail method works', ()=>{
-    const manager = new Manager('Joel',2,'joel@email.com');
-
-    expect(manager.getEmail()).toBe('joel@email.com');
-})
-
-test('checks to see if officeNumber method',()=>{
-    const manager = new Manager('Joel',2,'joel@email.com', 821);
-
-    expect(manager.getOfficeNumber()).toEqual(821);
-})
-
-test('checks to see if getRole method works', ()=>{
-    const manager = new Manager('Joel',2,'joel@email.com');
-
-    expect(manager.getRole()).toBe('Manager');
-})
