6e9e2391c863fb6fbdc495cfcef955a3729af7bc
night <yseddiq@gmail.com>
2022-09-14 14:08:03 -0400

    refactored engineer and intern  test, and added getName getID and get email methords to Engineer.js and Intern.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,24 +1,12 @@
 const Engineer = require('../lib/Engineer.js');
 
-test('checks to see if Engineer inherited properties from Employee',()=>{
-    const engineer = new Engineer ('Maria', 3, 'maria@email.com');
+test('checks to see if Engineer class has all the propeties and all methods work.',()=>{
+    const engineer = new Engineer ('Maria', 3, 'maria@email.com', 'mariaGitHub');
 
     expect(engineer.name).toBe('Maria');
     expect(engineer.id).toEqual(3);
     expect(engineer.email).toBe('maria@email.com');
-});
-test('checks if github property was succesfully added to Engineer',()=>{
-    const engineer = new Engineer('Maria', 3, 'maria@email.com', 'mariaGitHub');
-
     expect(engineer.github).toBe('mariaGitHub');
-});
-test('checks if getGithub method works',()=>{
-    const engineer= new Engineer('Maria',3,'maria@email.com','mariaGithub');
-    
-    expect(engineer.getGithub()).toBe('mariaGithub');
-});
-test('checks if getRole method works.', ()=>{
-    const engineer= new Engineer('Maria',3,'maria@email.com','mariaGithub');
-
+    expect(engineer.getGithub()).toBe('mariaGitHub');
     expect(engineer.getRole()).toBe('Engineer');
 });
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,25 +1,12 @@
 const Intern = require('../lib/Intern.js')
 
 test('checks to see if Engineer inherited properties from Employee',()=>{
-    const intern = new Intern ('Larry', 4, 'larry@email.com');
+    const intern = new Intern ('Larry', 4, 'larry@email.com', 'Montclair State');
 
     expect(intern.name).toBe('Larry');
     expect(intern.id).toEqual(4);
     expect(intern.email).toBe('larry@email.com');
-});
-test('checks if school propery was sueccesfully added',()=>{
-    const intern = new Intern ('Larry', 4, 'larry@email.com', 'Montclair State');
-
-    expect(intern.school).toBe('Montclair State')
-});
-test('checks if getSchool method works.',()=>{
-    const intern = new Intern ('Larry', 4, 'larry@email.com', 'Montclair State');
-
+    expect(intern.school).toBe('Montclair State');
     expect(intern.getSchool()).toBe('Montclair State');
-});
-
-test('checks if getRole method works',()=>{
-    const intern = new Intern ('Larry', 4, 'larry@email.com', 'Montclair State');
-
     expect(intern.getRole()).toBe('intern');
-})
+});
